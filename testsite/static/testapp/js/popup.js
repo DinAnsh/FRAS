1fe20db657151abffc8c5461aa6ff3ae14879978
dipendrasingh100 <dipsingh9950@gmail.com>
2023-03-17 00:31:55 +0530

    Added responsive login and register popup

--- testsite/static/testapp/js/popup.js
+++ testsite/static/testapp/js/popup.js
@@ -1,15 +1,33 @@
+// for registration
 function openModal() {
-    document.getElementById("myModal").style.display = "block";
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// for login
+function openModal2() {
+  document.getElementById("myModal2").style.display = "block";
+}
+
+function closeModal2() {
+  document.getElementById("myModal2").style.display = "none";
+}
+
+
+window.onclick = function (event) {
+  // console.log(event.target)
+  if (
+    event.target == document.getElementById("cbtn") ||
+    event.target == document.getElementById("content") 
+  ) {
+    closeModal();
   }
-  
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+
+  else if (event.target == document.getElementById("cbtn2") ||
+  event.target == document.getElementById("content2")) {
+    closeModal2();
   }
-  
-  window.onclick = function(event) {
-    // console.log(event.target)
-    if (event.target == document.getElementById("cbtn") || event.target == document.getElementById("content")){
-      closeModal();
-    }
-  }
-  
+};
